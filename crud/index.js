da4abe756f832fb59de556e0fbc732b195bf9501
kikemaya <enrique.mg.cd@gmail.com>
2022-08-23 21:36:44 -0500

    2da clase, 3era semana. Practica CRUD, Create and read...

--- crud/index.js
+++ crud/index.js
@@ -1,15 +1,21 @@
 "use strict"
 
+const { v4: uuidv4 } = require('uuid');
+
 const d = document
 
+// capturando los nodos de html con javascript
 const input_todo = d.getElementById('input-todo')
 const btn_add = d.getElementById('btn-add')
 const content_w = d.getElementById('content-wrapper')
 
+// url base para consumir api
 const url = 'http://localhost:3000/data'
 
-const printTask = (task) => {
+// funcion que imprime elementos li y button en el dom
+const printTask = (task, id) => {
   let li = d.createElement('li')
+  li.dataset.id = id
   li.textContent = task
 
   const btn_1 = d.createElement('button')
@@ -18,7 +24,7 @@ const printTask = (task) => {
   btn_2.textContent = 'Eliminar'
 
   btn_1.addEventListener('click', () => {
-    console.log('Editando...');
+    putData()
   })
 
   btn_2.addEventListener('click', () => {
@@ -30,11 +36,54 @@ const printTask = (task) => {
   content_w.appendChild(li)
 }
 
+// funcion que obtiene datos de la api
 const getData = () => {
-  fetch(url)
+  return fetch(url)
     .then(response => response.json())
-    .then(data => data)
+    .then(data => {
+      data.forEach(element => {
+        printTask(element.task, element.id)
+      });
+    })
+    .catch(error => console.error(error))
+}
+
+// funcion que envia datos a la api
+const postData = (task) => {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'id': uuidv4(),
+      'task': task
+    })
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+}
+// crear la logica como te imagines que tiene que ser para editar una tarea
+const putData = () => {
+  return fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'task': 'Hola, soy una tarea nueva'
+    })
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
     .catch(error => console.error(error))
 }
 
 getData()
+
+btn_add.addEventListener('click', () => {
+  return postData(input_todo.value)
+})
+
+
