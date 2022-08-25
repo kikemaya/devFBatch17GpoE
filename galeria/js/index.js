e6418dbf9fe4da3ece322f769992dbf627f46f7d
kikemaya <enrique.mg.cd@gmail.com>
2022-08-24 21:32:42 -0500

    arribaaaa...

--- galeria/js/index.js
+++ galeria/js/index.js
@@ -1,2 +1,32 @@
 "use strict"
 
+const d = document
+
+const galleryContainer = d.getElementById('gallery-container')
+
+const url = "https://picsum.photos/v2/list"
+
+const printGallery = () => {
+  const div = d.createElement('div')
+  div.classList.add("grid-item")
+
+  galleryContainer.appendChild(div)
+}
+
+printGallery()
+printGallery()
+
+const getImages = async () => {
+  const response = await fetch(url)
+  const data = await response.json()
+
+  console.log(data);
+
+  data.forEach(element => {
+    printGallery()
+  });
+}
+
+getImages()
+
+// colocar la imagen en el cuadrado
